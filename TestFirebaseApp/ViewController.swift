58bc6fe1a2950d14cf3dfc89b32713175bacf640
Siwawes Wongcharoen <siwaoh@gmail.com>
2016-07-24 13:51:22 +0700

    Initial Commit

---
+++ TestFirebaseApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestFirebaseApp
+//
+//  Created by Siwawes Wongcharoen on 7/24/2559 BE.
+//  Copyright © 2559 puuga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
